ac0e236d6f37f5ba649ddf4d847ba5c8eb32c0c5
student <student@lab.edu>
2017-10-14 09:24:47 -0400

    First commit of ball class

---
+++ src/ball.java
@@ -0,0 +1,19 @@
+/*
+ Created by Garth Lezama on 14/10/2017.
+*/
+public class ball {
+
+    // instance variables
+    private double rx, ry;        // position
+    private double vx, vy;        // velocity
+    private final double radius;  // radius
+
+    // constructor
+    public ball() {
+        rx = 0.0;
+        ry = 0.0;
+        vx =  0.0;
+        vy =  0.0;
+        radius = 1.0;
+    }
+}
